cb725d30ca1513144746d7185d7ba584cf380a5a
jpswt <joel_swt@yahoo.com>
2023-04-08 21:04:37 -0500

    fix multer

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const multer = require('multer');
 const authRoute = require('./routes/auth');
 const usersRoute = require('./routes/user');
 const productsRoute = require('./routes/product');
