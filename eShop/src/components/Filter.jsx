24a239c64664cea13f80fc7aa8d1195461985278
jpswt <joel_swt@yahoo.com>
2023-04-13 11:20:27 -0500

    finish products page

--- eShop/src/components/Filter.jsx
+++ eShop/src/components/Filter.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import '../styles/Filter.css';
 
 const Filter = () => {
 	return (
@@ -7,10 +7,23 @@ const Filter = () => {
 			<div className="filter-container">
 				<div className="filter-item">
 					<label>Filter Products:</label>
-					<select name="cameras" id="cameras" defaultValue="Color">
-						<option value="Black"></option>
-						<option value="White"></option>
-						<option value="Gray"></option>
+					<select name="cameras" id="cameras">
+						<option value="" disabled selected>
+							Choose a Color
+						</option>
+						<option value="black">Black</option>
+						<option value="white">White</option>
+						<option value="gray">Gray</option>
+					</select>
+				</div>
+				<div className="filter-item">
+					<label>Sort Products:</label>
+					<select name="cameras" id="cameras">
+						<option value="" selected>
+							Latest
+						</option>
+						<option>Price Asc</option>
+						<option>Price Desc</option>
 					</select>
 				</div>
 			</div>
