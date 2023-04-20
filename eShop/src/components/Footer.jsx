26827c42119fdeea5832f40d157f51acd775fbef
jpswt <joel_swt@yahoo.com>
2023-04-19 20:32:19 -0500

    fix footer

--- eShop/src/components/Footer.jsx
+++ eShop/src/components/Footer.jsx
@@ -1,43 +1,105 @@
+import { useState, useEffect } from 'react';
 import '../scss/styles/Footer.css';
 
 const Footer = () => {
+	const [width, setWidth] = useState(window.innerWidth);
+	const breakpoint = 1000;
+
+	useEffect(() => {
+		const handleResize = () => setWidth(window.innerWidth);
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
+
 	return (
 		<div className="footer-container">
-			<div className="footer-left">
-				<i className="fa-brands fa-square-facebook fa-2xl"></i>
-				<i className="fa-brands fa-square-instagram fa-2xl"></i>
-				<i className="fa-brands fa-square-twitter fa-2xl"></i>
-				<i className="fa-brands fa-square-youtube fa-2xl "></i>
-			</div>
-			<div className="footer-center">
-				<div className="links-title">Helpful Links</div>
-				<ul className="footer-links">
-					<li className="footer-link">Home</li>
-					<li className="footer-link">My Account</li>
-					<li className="footer-link">Cart</li>
-					<li className="footer-link">Order Tracking</li>
-					<li className="footer-link">Wishlist</li>
-					<li className="footer-link">Audio</li>
-					<li className="footer-link">Video</li>
-					<li className="footer-link">Photography</li>
-				</ul>
-			</div>
-			<div className="footer-right">
-				<ul className="footer-contact">
-					<span className="contact-item">
-						<i className="fa-solid fa-location-dot"></i>
-						<li className="contact-info">123 Main St Anyhere, USA</li>
-					</span>
-					<span className="contact-item">
-						<i className="fa-solid fa-square-phone"></i>
-						<li className="contact-info">555-867-5309</li>
-					</span>
-					<span className="contact-item">
-						<i className="fa-solid fa-envelope"></i>
-						<li className="contact-info">eShop@eshop.com</li>
-					</span>
-				</ul>
-			</div>
+			{width > breakpoint ? (
+				<>
+					<div className="footer-navlinks">
+						<div className="links-title">Helpful Links</div>
+						<ul className="footer-links">
+							<li className="footer-link">Home</li>
+							<li className="footer-link">My Account</li>
+							<li className="footer-link">Cart</li>
+							<li className="footer-link">Orders</li>
+							<li className="footer-link">Wishlist</li>
+							<li className="footer-link">Decks</li>
+							<li className="footer-link">Trucks</li>
+							<li className="footer-link">Wheels</li>
+							<li className="footer-link">Hardware</li>
+						</ul>
+					</div>
+					<div className="footer-social">
+						<div className="links-title">Social</div>
+						<div className="social-container">
+							<i className="fa-brands fa-square-facebook fa-2xl ">
+								<span className="text"></span>
+							</i>
+							<i className="fa-brands fa-square-instagram fa-2xl"></i>
+							<i className="fa-brands fa-square-twitter fa-2xl"></i>
+							<i className="fa-brands fa-square-youtube fa-2xl "></i>
+						</div>
+					</div>
+					<div className="footer-contact-info">
+						<div className="links-title contact-title">Contact Us</div>
+						<ul className="footer-contact">
+							<span className="contact-item">
+								<i className="fa-solid fa-location-dot"></i>
+								<li className="contact-info">123 Main St Anyhere, USA</li>
+							</span>
+							<span className="contact-item">
+								<i className="fa-solid fa-square-phone"></i>
+								<li className="contact-info">555-867-5309</li>
+							</span>
+							<span className="contact-item">
+								<i className="fa-solid fa-envelope"></i>
+								<li className="contact-info">eShop@eshop.com</li>
+							</span>
+						</ul>
+					</div>
+				</>
+			) : (
+				<>
+					<div className="footer-navlinks">
+						<div className="links-title">Helpful Links</div>
+
+						<ul className="footer-links">
+							<li className="footer-link">Home</li>
+							<li className="footer-link">My Account</li>
+							<li className="footer-link">Cart</li>
+							<li className="footer-link">Track Order</li>
+							<li className="footer-link">Wishlist</li>
+							<li className="footer-link">Decks</li>
+							<li className="footer-link">Trucks</li>
+							<li className="footer-link">Wheels</li>
+							<li className="footer-link">Hardware</li>
+						</ul>
+					</div>
+					<div className="footer-contact-info">
+						<div className="links-title">Contact Us</div>
+						<ul className="footer-contact">
+							<span className="contact-item">
+								<i className="fa-solid fa-location-dot"></i>
+								<li className="contact-info">123 Main St Anyhere, USA</li>
+							</span>
+							<span className="contact-item">
+								<i className="fa-solid fa-square-phone"></i>
+								<li className="contact-info">555-867-5309</li>
+							</span>
+							<span className="contact-item">
+								<i className="fa-solid fa-envelope"></i>
+								<li className="contact-info">eShop@eshop.com</li>
+							</span>
+						</ul>
+					</div>
+					<div className="footer-social">
+						<i className="fa-brands fa-square-facebook fa-2xl"></i>
+						<i className="fa-brands fa-square-instagram fa-2xl"></i>
+						<i className="fa-brands fa-square-twitter fa-2xl"></i>
+						<i className="fa-brands fa-square-youtube fa-2xl "></i>
+					</div>
+				</>
+			)}
 		</div>
 	);
 };
