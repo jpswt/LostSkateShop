3b17848714f8881c6e4a2ed37621b1b9aa734ed3
jpswt <joel_swt@yahoo.com>
2023-04-22 00:22:26 -0500

    sort and filter working

--- eShop/src/components/Filter.jsx
+++ eShop/src/components/Filter.jsx
@@ -1,76 +1,32 @@
-import { useEffect, useState } from 'react';
 import '../scss/styles/Filter.css';
-import axios from 'axios';
 
-const Filter = ({ filters, sort, setSort, handleFilter, category }) => {
-	console.log(filters);
-	console.log(sort);
-
-	const [products, setProducts] = useState([]);
-	const [filteredProducts, setFilteredProducts] = useState([]);
-	const [options, setOptions] = useState([]);
-
-	useEffect(() => {
-		const getProducts = async () => {
-			try {
-				const response = await axios.get(
-					category
-						? `${import.meta.env.VITE_DB_URI}/products?category=${category}`
-						: `${import.meta.env.VITE_DB_URI}/products`
-				);
-				setProducts(response.data);
-				const results = [];
-				response.data.forEach((item, index) => {
-					results.push({
-						key: item.manufacturer,
-						value: item.manufacturer,
-					});
-
-					setOptions([{ key: 'Select Manufacturer', value: '' }, ...results]);
-				});
-			} catch (err) {}
-		};
-		getProducts();
-	}, [category]);
-	console.log(options);
-
+const Filter = ({
+	filters,
+	sort,
+	setSort,
+	handleFilter,
+	options,
+	category,
+}) => {
 	return (
 		<div className="filter">
-			<h2>Cameras</h2>
+			<h2>{category.toUpperCase()}</h2>
 			<div className="filter-container">
 				<div className="filter-item">
 					<label>Filter Products:</label>
-					<select name="manufacturer" id="cameras" onChange={handleFilter}>
-						{filteredProducts.map((item) => {
-							<option value={`${item.manufacturer}`}>
-								${item.manufacturer}
-							</option>;
-						})}
-						{/* <option value="">Choose a Manufacturer</option>
-						<option value="Santa Cruz">Santa Cruz</option>
-						<option value="white">White</option>
-						<option value="gray">Gray</option> */}
-					</select>
-				</div>
-				<div className="filter-item">
-					<label>Filter Products:</label>
-					<select name="color" id="cameras" onChange={handleFilter}>
-						<option value="">Choose a Color</option>
-						<option value="black">Black</option>
-						<option value="white">White</option>
-						<option value="gray">Gray</option>
+					<select name="manufacturer" id="manufacturer" onChange={handleFilter}>
+						<option value="">All Brands</option>
+						{options.map((option, index) => (
+							<option key={index} value={option.value}>
+								{option.value}
+							</option>
+						))}
 					</select>
 				</div>
 				<div className="filter-item">
 					<label>Sort Products:</label>
-					<select
-						name="price"
-						id="cameras"
-						onChange={(e) => setSort(e.target.value)}
-					>
-						<option value="latest" selected>
-							Latest
-						</option>
+					<select onChange={(e) => setSort(e.target.value)}>
+						<option value="latest">Latest</option>
 						<option value="asc">Price Asc</option>
 						<option value="desc">Price Desc</option>
 					</select>
--- eShop/src/components/Products.jsx
+++ eShop/src/components/Products.jsx
@@ -1,47 +1,16 @@
 import '../scss/styles/Products.css';
 import { popProducts } from '../assets/data/data';
 import ProductItems from './ProductItems';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 
-const Products = ({ category, filters, sort }) => {
-	const [products, setProducts] = useState([]);
-	const [filteredProducts, setFilteredProducts] = useState([]);
+const Products = ({ category, filters, sort, products, filteredProducts }) => {
+	console.log(category, filters, sort);
 
-	useEffect(() => {
-		const getProducts = async () => {
-			try {
-				const response = await axios.get(
-					category
-						? `${import.meta.env.VITE_DB_URI}/products?category=${category}`
-						: `${import.meta.env.VITE_DB_URI}/products`
-				);
-				setProducts(response.data);
-			} catch (err) {}
-		};
-		getProducts();
-	}, [category]);
-
-	useEffect(() => {
-		category &&
-			setFilteredProducts(
-				products.filter((item) =>
-					Object.entries(filters).every(([key, value]) =>
-						item[key].includes(value)
-					)
-				)
-			);
-	}, [products, category, filters]);
-
-	console.log(products);
 	return (
-		<>
-			<div className="products-container">
-				{filteredProducts.map((product) => (
-					<ProductItems product={product} key={product.id} />
-				))}
-			</div>
-		</>
+		<div className="products-container">
+			{filteredProducts.map((product) => (
+				<ProductItems product={product} key={product.id} />
+			))}
+		</div>
 	);
 };
 
