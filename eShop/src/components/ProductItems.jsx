16f076ef56d1fcc516a353de16f27e2613601f24
jpswt <joel_swt@yahoo.com>
2023-05-08 22:55:52 -0500

    fix items per product

--- eShop/src/components/ProductItems.jsx
+++ eShop/src/components/ProductItems.jsx
@@ -1,37 +1,7 @@
-// import { useRef, useState } from 'react';
-// import useLocalStorage from 'react-use-localstorage';
 import { Link } from 'react-router-dom';
-// import { addProduct } from '../redux/cartRedux';
-// import { useDispatch } from 'react-redux';
 import '../scss/styles/ProductItems.css';
 
 const ProductItems = ({ product }) => {
-	// const [quantity, setQuantity] = useState(1);
-	// const [storage, setStorage] = useLocalStorage(
-	// 	'favorite',
-	// 	JSON.stringify([]) || null
-	// );
-
-	// const storeArray = useRef(JSON.parse(storage));
-	// const isLiked = storeArray.current.includes(product._id);
-
-	// const handleToggle = () => {
-	// 	if (!isLiked) {
-	// 		storeArray.current.push(product._id);
-	// 		setStorage(JSON.stringify(storeArray.current));
-	// 	} else {
-	// 		const indexIsLiked = storeArray.current.indexOf(product._id);
-	// 		storeArray.current.splice(indexIsLiked, 1);
-	// 		setStorage(JSON.stringify(storeArray.current));
-	// 	}
-	// };
-
-	// const dispatch = useDispatch();
-
-	// const handleCart = () => {
-	// 	dispatch(addProduct({ ...product, quantity }));
-	// };
-
 	return (
 		<div className="product">
 			<div className="product-container">
