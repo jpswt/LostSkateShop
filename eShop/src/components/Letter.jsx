304148a54f675732065d453bc9fbd2abbc25f607
jpswt <joel_swt@yahoo.com>
2023-05-11 14:37:49 -0500

    revert newsletter

--- eShop/src/components/Letter.jsx
+++ eShop/src/components/Letter.jsx
@@ -4,11 +4,6 @@ import '../scss/styles/Letter.css';
 const Letter = () => {
 	return (
 		<div className="letter-container">
-			<img
-				className="banner-img"
-				src="https://images.unsplash.com/photo-1607639694361-fe08243bf70c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-				alt=""
-			/>
 			<div className="title">
 				<h2>Newsletter</h2>
 			</div>
