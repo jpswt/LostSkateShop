f8c9ee7e65291ab8662fcd73776e476ae17ab80e
jpswt <joel_swt@yahoo.com>
2023-05-13 11:38:45 -0500

    fix backend

--- backend/index.js
+++ backend/index.js
@@ -43,6 +43,15 @@ app.options('*', cors());
 
 // app.use(express.json());
 
+app.use(function (req, res, next) {
+	if (req.path.length > 1 && /\/$/.test(req.path)) {
+		var query = req.url.slice(req.path.length);
+		res.redirect(301, req.path.slice(0, -1) + query);
+	} else {
+		next();
+	}
+});
+
 app.use('/api/auth', authRoute);
 app.use('/api/users', usersRoute);
 app.use('/api/products', productsRoute);
