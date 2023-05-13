5b592bbb8cdfdc9f270d186d57abee4eb5f930ec
jpswt <joel_swt@yahoo.com>
2023-05-13 11:52:29 -0500

    fix backend

--- backend/index.js
+++ backend/index.js
@@ -21,38 +21,15 @@ app.use(cors());
 app.use('/api/stripe', stripeWebHookRoute);
 app.use((req, res, next) => {
 	if (req.originalUrl === '/webhook') {
-		next(); // Do nothing with the body because I need it in a raw state.
+		next();
 	} else {
-		express.json()(req, res, next); // ONLY do express.json() if the received request is NOT a WebHook from Stripe.
+		express.json()(req, res, next);
 	}
 });
 app.options('*', cors());
 
-// app.use(function (req, res, next) {
-// 	res.header('Access-Control-Allow-Origin', '*');
-// 	res.header(
-// 		'Access-Control-Allow-Methods',
-// 		'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-// 	);
-// 	res.header(
-// 		'Access-Control-Allow-Headers',
-// 		'Content-Type, Content-Length, Authorization, Accept, X-Requested-With'
-// 	);
-// 	next();
-// });
-
-// app.use(express.json());
-
 app.use('/api/auth', authRoute);
 app.use('/api/users', usersRoute);
-app.use(function (req, res, next) {
-	if (req.path.length > 1 && /\/$/.test(req.path)) {
-		var query = req.url.slice(req.path.length);
-		res.redirect(301, req.path.slice(0, -1) + query);
-	} else {
-		next();
-	}
-});
 app.use('/api/products', productsRoute);
 app.use('/api/orders', orderRoute);
 app.use('/api/checkout', stripeRoute);
